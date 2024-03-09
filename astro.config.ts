d985d1c5d11fce91242d97ac6f051d13d2d4eafe
christorres-dev <christian.gabriel.torres@depalmastudios.com>
2024-03-08 22:50:59 -0500

    🔧 Add and update condig files

---
+++ astro.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "astro/config"
+
+import tailwind from "@astrojs/tailwind"
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [tailwind()],
+	build: {
+		inlineStylesheets: "always",
+	},
+	output: "hybrid",
+	vite: {
+		build: {
+			cssMinify: "lightningcss",
+		},
+		ssr: {
+			noExternal: ["path-to-regexp"],
+		},
+	},
+})
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,33 @@
-import animations from '@midudev/tailwind-animations'
+import animations from "@midudev/tailwind-animations"
+import tailwindTheme from "tailwindcss/defaultTheme"
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {}
-  },
-  plugins: [animations]
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				title: ["Montserrat", ...tailwindTheme.fontFamily.mono],
+				subTitle: ["Lora", ...tailwindTheme.fontFamily.serif],
+				body: ["Hind\\ Madurai", ...tailwindTheme.fontFamily.sans],
+			},
+			colors: {
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)",
+				tertiary: "var(--color-tertiary)",
+				accent: "var(--color-accent)",
+			},
+			screens: {
+				xs: "360px",
+				...tailwindTheme.screens,
+			},
+		},
+	},
+
+	plugins: [
+		animations,
+		function ({ addVariant }) {
+			addVariant("any-hover", "@media (any-hover: hover) { &:hover }")
+		},
+	],
 }
