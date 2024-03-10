b0072eaada05ad613bb199ad1cb55dbe647064be
christorres-dev <christian.gabriel.torres@depalmastudios.com>
2024-03-10 00:17:24 -0500

    🚀 Changes for deploy

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,20 @@
+import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 
-import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
+	output: "hybrid",
 	build: {
 		inlineStylesheets: "always",
 	},
-	output: "hybrid",
 	vite: {
 		build: {
 			cssMinify: "lightningcss",
