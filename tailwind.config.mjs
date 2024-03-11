0d9bf73cae88629421e7238070f6a8397418859a
christorres-dev <christian.gabriel.torres@depalmastudios.com>
2024-03-10 19:23:07 -0500

    💄 Update tailwind colors and other values

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,11 +11,17 @@ export default {
 				subtitle: ["Lora", ...tailwindTheme.fontFamily.serif],
 				body: ["Hind\\ Madurai", ...tailwindTheme.fontFamily.sans],
 			},
+			fontSize: {
+				topper: "var(--topper-font-size)",
+				header: "var(--header-font-size)",
+				body: "var(--body-font-size)",
+			},
 			colors: {
-				primary: "var(--color-primary)",
-				secondary: "var(--color-secondary)",
-				tertiary: "var(--color-tertiary)",
-				accent: "var(--color-accent)",
+				"primary": "var(--color-primary)",
+				"secondary": "var(--color-secondary)",
+				"secondary-accent": "var(--color-secondary-accent)",
+				"tertiary": "var(--color-tertiary)",
+				"accent": "var(--color-accent)",
 			},
 			screens: {
 				xs: "360px",
@@ -24,6 +30,7 @@ export default {
 			boxShadow: {
 				"nav": "0 0 20px rgba(0, 0, 0, 0.1)",
 				"nav-dark": "0 0 20px rgba(252, 252, 252, 0.6)",
+				"service-card": "0px 8px 50px rgba(149, 157, 165, 0.4)",
 			},
 		},
 	},
